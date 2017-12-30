ec9f3b72c715da275f95205251d715646261ab7b
atong007 <hong3801505@126.com>
2017-12-30 22:08:46 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/atong007/blog/coolweather/db/Country.java
@@ -0,0 +1,50 @@
+package com.atong007.blog.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by atong007 on 2017/12/30.
+ */
+
+public class Country extends DataSupport {
+
+    private int id;
+
+    private String countryName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
